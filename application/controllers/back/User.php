7cf36f7c0fac075a63a4fc159e111d9ad5a34172
raphael_aji <raphaelaji69@gmail.com>
2018-08-10 21:10:11 +0700

    update

--- application/controllers/back/User.php
+++ application/controllers/back/User.php
@@ -36,6 +36,49 @@ class User extends CI_Controller {
 		$this->load->view('back/user/user_profile',$data);
 		$this->load->view('layout/back/footer');
 	}
+
+	public function tambah() {
+		$data['data_user'] = $this->M_user->get_user();
+		$data['data_level'] = $this->M_user->get_level();
+
+		$this->load->view('layout/back/header',$data);
+		$this->load->view('layout/back/sidebar',$data);
+		$this->load->view('back/user/tambah_user',$data);
+		$this->load->view('layout/back/footer',$data);
+	
+	}
+
+	public function tambah_aksi(){
+		//print_r($_POST);exit;
+		$this->form_validation->set_rules('username','username','required');
+		$this->form_validation->set_rules('password','password','required');
+		$this->form_validation->set_rules('jenis_kelamin','jenis_kelamin','required');
+		if($this->form_validation->run() == false){
+			$this->session->set_flashdata('sukses', "<div class=\"alert alert-danger\" id=\"alert\"><i class=\"\"><strong>error!</strong><br></i> Registrasi Gagal, Silahkan masukkan jenis kelamin username dan password</div>");
+		redirect('back/user/tambah');
+		}else{
+			$data_cari = array(
+			'username' => $this->input->post('username'),
+			);
+		$cek = $this->M_user->get_cari_username1($data_cari);
+		if ($cek > 0){
+			$this->session->set_flashdata('sukses', "<div class=\"alert alert-danger\" id=\"alert\"><i class=\"glyphicon glyphicon-ok\"><strong>error!</strong><br></i> Registrasi Gagal, username anda sudah ada</div>");
+		redirect('back/user/tambah');
+		}else{
+		$data_user = array(
+			'nama' => $this->input->post('nama'),
+			'pass' => $this->input->post('password'),
+			'jenis_kelamin' => $this->input->post('jenis_kelamin'),
+			'username' => $this->input->post('username'),
+			'password' => md5($this->input->post('password')),
+			'id_level' => $this->input->post('id_level')
+			);
+		//print_r($data_user);exit;
+		$this->M_user->tambah($data_user);
+		$this->session->set_flashdata('sukses', "<div class=\"alert alert-success\" id=\"alert\"><i class=\"\"></i>Berhasil menambah data user </div>");
+		redirect('back/user/semua_user');}}
+	}
+
 	public function edit($id) {	
 
 		 //$id= $this->session->userdata('id'); 
@@ -62,7 +105,6 @@ class User extends CI_Controller {
 		}else{
 	$data_user = array(
 			'id_user' => $this->input->post('id_user'),
-			'kode_pendaftaran' => $this->input->post('kode_pendaftaran'),
 			'id_level' => $this->input->post('id_level'),
 			'nama' => $this->input->post('nama'),
 			'pass' => $this->input->post('password'),
@@ -72,32 +114,11 @@ class User extends CI_Controller {
 			);
 //print_r($data_user);exit;
 	$this->M_user->edit($data_user);
-	$this->session->set_flashdata('sukses', "<div class=\"alert alert-success\" id=\"alert\"><i class=\"\"></i> Edit User Berhasil</div>");
-	redirect('back/user/edit');}}
-	else {
-		$this->form_validation->set_rules('username','username','required');
-		$this->form_validation->set_rules('password','password','required');
-		$this->form_validation->set_rules('jenis_kelamin','jenis_kelamin','required');
-		if($this->form_validation->run() == false){
-			$this->session->set_flashdata('sukses', "<div class=\"alert alert-danger\" id=\"alert\"><i class=\"\"><strong>error!</strong><br></i> Edit Data Gagal, Silahkan masukkan jenis kelamin username dan password</div>");
-	redirect('back/user/edit');
-		}else{
-	$data_user = array(
-			'id_user' => $this->input->post('id_user'),
-			'kode_pendaftaran' => $this->input->post('kode_pendaftaran'),
-			'id_level' => '2',
-			'nama' => $this->input->post('nama'),
-			'pass' => $this->input->post('password'),
-			'jenis_kelamin' => $this->input->post('jenis_kelamin'),
-			'username' => $this->input->post('username'),
-			'password' => md5($this->input->post('password')),
-			);
-//print_r($data_user);exit;
-	$this->M_user->edit($data_user);
-	$this->session->set_flashdata('sukses', "<div class=\"alert alert-success\" id=\"alert\"><i class=\"\"></i> Edit User Berhasil</div>");
-	redirect('back/user/edit');}
+	$this->session->set_flashdata('sukses', "<div class=\"alert alert-success\" id=\"alert\"><i class=\"\"></i> Data User Berhasil Diubah</div>");
+	redirect('back/user/semua_user');}}
+	
 	}
-	}
+
 	public function semua_user($offset=0)
 	{
 			$jml = $this->db->get('tb_user');
@@ -143,6 +164,7 @@ class User extends CI_Controller {
 			$this->load->view('back/user/semua_user',$data);
 			$this->load->view('layout/back/footer');
 	}
+
 	public function delete($id) {
 		$this->M_user->delete($id);
 		$this->session->set_flashdata("pesan", "<div class=\"alert alert-success\" id=\"alert\"><i class=\"glyphicon glyphicon-ok\"></i> Data berhasil dihapus</div>");
